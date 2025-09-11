6c51d7ae83d732732e4dea45ea0cd1db36a85f02
franco43424 <f.fajardo2@alumnos.santotomas.cl>
2025-09-11 01:14:54 -0300

    5 layout lista primer commit de interfaz.

--- app/src/main/java/com/example/proyecto1_franco_android/activity_consulta_ubi.java
+++ app/src/main/java/com/example/proyecto1_franco_android/activity_consulta_ubi.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_pokeball extends AppCompatActivity {
+public class activity_consulta_ubi extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_pokeball);
+        setContentView(R.layout.activity_consulta_ubi);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
